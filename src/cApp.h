4e1453c7e4b7fe27c3eee76dc68861465fb41650
kyledavis124 <34011041+kyledavis124@users.noreply.github.com>
2021-07-07 19:38:48 -0400

    src: changed default design pattern

---
+++ src/cApp.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include "cMain.h"
+
+class cApp : public wxApp
+{
+public:
+	cApp();
+	~cApp();
+
+private:
+	cMain *m_frame1 = nullptr;
+
+public:
+	virtual bool OnInit();
+};
---
+++ src/cMain.cpp
@@ -0,0 +1,9 @@
+#include "cMain.h"
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "hello, world - wxWidgets!")
+{
+}
+
+cMain::~cMain()
+{
+}
